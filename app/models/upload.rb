d68cbb04727461c601370271b70a5c730de71887
David Mrnustik <david.mrnustik@hotmail.com>
2016-05-21 17:22:13 +0200

    Image validation is the last

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,5 +6,9 @@ class Upload
   
   validates_presence_of :quality, message: "No ha puesto el valor de calidad: 30-100"
   validates_numericality_of :quality, greater_than: 29, less_than: 101, message: "No ha puesto número entre 30 - 100"
-  validates_presence_of :image, message: "No ha subido ninguna imagen."
+
+  with_options if: :quality do |qty|
+    qty.validates :image, presence: {message: "No ha subido ninguna imagen"}
+  end
+
 end
